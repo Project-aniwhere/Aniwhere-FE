048ddd3a883dbfd14e61f86953a903a1222ff00f
JK <74600256+Programmer-JK@users.noreply.github.com>
2024-10-24 22:19:44 +0900

    [FEAT] footer generate #

---
+++ src/common/footer/footer.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const Footer = () => {
+  return (
+    <footer className='mt-20 p-5 w-full relative text-center bg-aniviolet3 text-white'>
+      <h2 className='font-bold text-3xl leading-snug'>Aniwhere</h2>
+    </footer>
+  );
+};
+
+export default Footer;
