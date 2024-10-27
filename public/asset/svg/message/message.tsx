8f270e925eb3fa72f19be3c00a7d80dfb5fb0804
JK <74600256+Programmer-JK@users.noreply.github.com>
2024-10-27 22:17:39 +0900

    [feat] svg 컴포넌트로 추출, font 파일 추가 #

---
+++ public/asset/svg/message/message.tsx
@@ -0,0 +1,19 @@
+import { SvgProps } from '@/type/svg/svgprops';
+
+const MessageSvg = ({}: SvgProps) => {
+  return (
+    <svg
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      stroke='white'
+    >
+      <path
+        d='M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z'
+        strokeWidth='2'
+      />
+    </svg>
+  );
+};
+
+export default MessageSvg;
