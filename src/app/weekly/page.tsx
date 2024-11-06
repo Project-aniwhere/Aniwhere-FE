60e95708a8ecbbe575bbde69c6961ada90e7d9f8
yujinyny <hyjinny00@gmail.com>
2024-11-06 18:31:43 +0900

    [CHORE] 요일별 작품 페이지 MainLayout 적용 #

--- src/app/weekly/page.tsx
+++ src/app/weekly/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import MainLayout from '@/component/common/layout/main-layout';
 import WeeklyContainer from '@/component/weekly/weekly-container';
 import WeeklyTitle from '@/component/weekly/weekly-title';
 import { WEEKLY_DUMMY } from '@/constant/dummy';
@@ -11,16 +12,18 @@ const WeeklyPage = () => {
   // todo. GET 요일별 애니 목록
 
   return (
-    <div className='w-full max-w-[1200px] mx-auto pt-[96px] pb-7 flex flex-col gap-10'>
-      <WeeklyTitle
-        selectedYear={selectedYear}
-        setSelectedYear={setSelectedYear}
-      />
-      <WeeklyContainer
-        weeklyList={WEEKLY_DUMMY.titleListMap}
-        currentDay={WEEKLY_DUMMY.dayOfWeek}
-      />
-    </div>
+    <MainLayout>
+      <div className='w-full max-w-[1200px] mx-auto pt-[100px] pb-10 flex flex-col gap-10'>
+        <WeeklyTitle
+          selectedYear={selectedYear}
+          setSelectedYear={setSelectedYear}
+        />
+        <WeeklyContainer
+          weeklyList={WEEKLY_DUMMY.titleListMap}
+          currentDay={WEEKLY_DUMMY.dayOfWeek}
+        />
+      </div>
+    </MainLayout>
   );
 };
 
