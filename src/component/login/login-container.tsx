3ebe845e95206379230b663917b505615ff3a37f
JK <74600256+Programmer-JK@users.noreply.github.com>
2024-11-07 21:16:35 +0900

    (feat) 로그인페이지 -> signup페이지로 이동하는 link 추가 #

--- src/component/login/login-container.tsx
+++ src/component/login/login-container.tsx
@@ -6,6 +6,7 @@ import HoverColorButton from '../common/button/hover-color-button';
 import UnderlineButton from '../common/button/hover-underline-button';
 import IconButton from '../common/button/icon-button';
 import DefaultInput from '../common/input/default-input';
+import Link from 'next/link';
 
 const LoginContainer = () => {
   return (
@@ -41,7 +42,9 @@ const LoginContainer = () => {
         </span>
         <span className='mt-2 text-gray-600 inline-flex'>
           <p>계정이 없으신가요?</p>
-          <UnderlineButton className='ml-1' text='회원가입' />
+          <Link href='/signup'>
+            <UnderlineButton className='ml-1' text='회원가입' />
+          </Link>
         </span>
       </div>
 
@@ -54,8 +57,13 @@ const LoginContainer = () => {
         </div>
 
         <div className='flex justify-center gap-4 mt-6'>
-          <IconButton Icon={KakaotalkSvg} height='3rem' width='3rem' />
-          <IconButton Icon={GoogleSvg} height='3rem' width='3rem' />
+          <IconButton>
+            <KakaotalkSvg height='3rem' width='3rem' />
+          </IconButton>
+
+          <IconButton>
+            <GoogleSvg height='3rem' width='3rem' />
+          </IconButton>
         </div>
       </div>
     </div>
