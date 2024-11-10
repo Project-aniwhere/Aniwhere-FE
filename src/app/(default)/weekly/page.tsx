836f85995706a158b9595067d147fa7ee621b2a5
leeguts <rmstjd333@gmail.com>
2024-11-11 00:55:03 +0900

    [FEAT] 디자인 별 레이아웃 분리

--- src/app/(default)/weekly/page.tsx
+++ src/app/(default)/weekly/page.tsx
@@ -4,26 +4,30 @@ import MainLayout from '@/component/common/layout/main-layout';
 import WeeklyContainer from '@/component/weekly/weekly-container';
 import WeeklyTitle from '@/component/weekly/weekly-title';
 import { WEEKLY_DUMMY } from '@/constant/dummy';
+import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 
 const WeeklyPage = () => {
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
 
-  // todo. GET 요일별 애니 목록
+  const { data } = useQuery({
+    queryKey: ['weekly', selectedYear],
+    queryFn: async () => {
+      return WEEKLY_DUMMY;
+    },
+  });
 
   return (
-    <MainLayout>
-      <div className='w-full max-w-[1200px] mx-auto pt-[100px] pb-10 flex flex-col gap-10'>
-        <WeeklyTitle
-          selectedYear={selectedYear}
-          setSelectedYear={setSelectedYear}
-        />
-        <WeeklyContainer
-          weeklyList={WEEKLY_DUMMY.titleListMap}
-          currentDay={WEEKLY_DUMMY.dayOfWeek}
-        />
-      </div>
-    </MainLayout>
+    <>
+      <WeeklyTitle
+        selectedYear={selectedYear}
+        setSelectedYear={setSelectedYear}
+      />
+      <WeeklyContainer
+        weeklyList={WEEKLY_DUMMY.titleListMap}
+        currentDay={WEEKLY_DUMMY.dayOfWeek}
+      />
+    </>
   );
 };
 
