f92649ec963acb7d2a3c4ae8181519dc6dbd168e
geun seong lee <geunseong_lee@tmax.co.kr>
2024-11-11 09:22:19 +0900

    [CHORE] 현재 쓰이지 않는 react-query 제거

--- src/app/(default)/weekly/page.tsx
+++ src/app/(default)/weekly/page.tsx
@@ -1,21 +1,12 @@
 'use client';
 
-import MainLayout from '@/component/common/layout/main-layout';
 import WeeklyContainer from '@/component/weekly/weekly-container';
 import WeeklyTitle from '@/component/weekly/weekly-title';
 import { WEEKLY_DUMMY } from '@/constant/dummy';
-import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 
 const WeeklyPage = () => {
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
-
-  const { data } = useQuery({
-    queryKey: ['weekly', selectedYear],
-    queryFn: async () => {
-      return WEEKLY_DUMMY;
-    },
-  });
 
   return (
     <>
