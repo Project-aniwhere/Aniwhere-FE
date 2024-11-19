8d76d5238bd4a1a8dd08747dca6f3b2dd11a3631
yujinyny <hyjinny00@gmail.com>
2024-11-19 20:01:59 +0900

    style: 상세 페이지 배너 배경 그라디언트 추가 #

--- src/component/detail/banner.tsx
+++ src/component/detail/banner.tsx
@@ -5,6 +5,7 @@ import sample1 from '@/asset/img/subslider/sample1.jpg';
 const DetailBanner = () => {
   return (
     <div className='relative'>
+      <div className='w-full h-full absolute z-10 bg-gradient-to-b from-transparent to-black/50'></div>
       <Image
         src={sample1.src}
         alt='배너'
@@ -13,8 +14,8 @@ const DetailBanner = () => {
         height={720}
         className='object-cover max-h-[480px]'
       />
-      <div className='w-full absolute bottom-0 flex justify-between items-end p-8'>
-        <div className='flex flex-col gap-2 text-white'>
+      <div className='w-full absolute bottom-0 z-20 flex justify-between items-end p-8 text-white'>
+        <div className='flex flex-col gap-2'>
           <p className='text-4xl font-bold'>스파이 패밀리 시즌1</p>
           <div className='flex gap-2 text-lg font-medium'>
             <div className='flex gap-1'>
@@ -25,10 +26,10 @@ const DetailBanner = () => {
             <span>25분</span>
           </div>
         </div>
-        <div className='flex flex-col items-center gap-1 text-gray-400'>
+        <button className='flex flex-col items-center gap-1 opacity-85'>
           <span>+</span>
           <span className='font-medium'>위시 리스트</span>
-        </div>
+        </button>
       </div>
     </div>
   );
