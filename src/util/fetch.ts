21c0d4869b73ac4751de937f8019b1c1768a1f00
geun seong lee <geunseong_lee@tmax.co.kr>
2024-11-25 15:30:55 +0900

    [FEAT] SSR 기반 커스텀 fetch 구현

---
+++ src/util/fetch.ts
@@ -0,0 +1,58 @@
+import { API_URL } from '@/constant/api-url';
+import { isServer } from '@tanstack/react-query';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+const getURL = () => {
+  if (process.env.NODE_ENV === 'production' && isServer) {
+    return 'http://localhost:8080';
+  }
+  return API_URL;
+};
+
+export const Fetch = async (input: RequestInfo | URL, init?: RequestInit) => {
+  return fetch(`${getURL()}/${input}`, init);
+};
+
+const FetchWithCookie = async (
+  input: RequestInfo | URL,
+  init?: RequestInit
+) => {
+  if (process.env.NODE_ENV === 'production' && isServer) {
+    return Fetch(input, {
+      ...init,
+      headers: {
+        'Content-Type': 'application/json',
+        Cookie: cookies().toString(),
+      },
+      credentials: 'include',
+    });
+  }
+  return Fetch(input, {
+    ...init,
+  });
+};
+
+export const FetchWithJWT = async (
+  input: RequestInfo | URL,
+  init?: RequestInit
+) => {
+  const res = await FetchWithCookie(input, init);
+
+  if (res.status === 401) {
+    const tokensRequest = await fetch('/reissue', init);
+    if (!tokensRequest.ok) return redirect('/login');
+
+    if (process.env.NODE_ENV === 'production' && isServer) {
+      const newCookies = tokensRequest.headers.getSetCookie();
+      newCookies.forEach((cookie) => {
+        const [name, value] = cookie.split('=');
+        cookies().set(name, value);
+      });
+    }
+
+    return FetchWithCookie(input, init);
+  } else {
+    return res;
+  }
+};
