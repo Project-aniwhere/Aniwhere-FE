8e62e62a93e4f348f06946a6a12d279cd596ce19
yujinyny <hyjinny00@gmail.com>
2024-12-03 18:07:18 +0900

    feat: next cofing remotePatterns 'media.kitsu.app' 추가 #

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        loader: "default",
-        remotePatterns: [{
-            protocol: "https",
-            hostname: "cloudfronturl",
-        }],
-    }
+  images: {
+    loader: 'default',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cloudfronturl',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.kitsu.app',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
