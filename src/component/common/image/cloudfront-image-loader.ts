c9e7563d2322f363af4c248addc82431f5d48b6f
leeguts <rmstjd333@gmail.com>
2024-12-10 21:08:48 +0900

    [FEAT] Add cloudfront image loader for image component

--- src/component/common/image/cloudfront-image-loader.ts
+++ src/component/common/image/cloudfront-image-loader.ts
@@ -8,9 +8,11 @@ const cloudfrontImageLoader = ({
   quality?: number;
 }) => {
   const url = new URL(`${src}`);
+  /*
   url.searchParams.set('format', 'auto');
   url.searchParams.set('width', width.toString());
   url.searchParams.set('quality', (quality || 75).toString());
+  */
   return url.href;
 };
 
