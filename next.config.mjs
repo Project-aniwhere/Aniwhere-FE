42adeab8a73eb6dbcc4d0f19a6c6f2b069223774
JK <74600256+Programmer-JK@users.noreply.github.com>
2024-12-22 16:08:21 +0900

    [fix] 새로고침 시 cookie 사라지는 이슈 해결 #

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,26 @@
-/** @type {import('next').NextConfig} */
+/* @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        loader: "default",
-        remotePatterns: [{
-            protocol: "https",
-            hostname: "cloudfronturl",
-        }],
-    }
+  images: {
+    loader: 'default',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cloudfronturl',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/reissue',
+        destination: 'http://15.164.142.195/api/auth/reissue',
+      },
+      {
+        source: '/api/:path*',
+        destination: 'http://15.164.142.195/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
