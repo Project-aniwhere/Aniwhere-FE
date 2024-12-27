298f1d9d961f879bf43a891fb04250272826bf45
yujinyny <hyjinny00@gmail.com>
2024-12-28 00:40:24 +0900

    style: 요일별 페이지 MainLayout 스타일 삭제 #

--- src/app/(default)/weekly/page.tsx
+++ src/app/(default)/weekly/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import MainLayout from '@/component/common/layout/main-layout';
 import MobileDailyConatiner from '@/component/weekly/daily-container-mobile';
 import WeeklyContainer from '@/component/weekly/weekly-container';
 import WeeklyTitle from '@/component/weekly/weekly-title';
@@ -19,25 +18,23 @@ const WeeklyPage = () => {
   // todo. GET 요일별 애니 목록
 
   return (
-    <MainLayout>
-      <div className='pt-5 pb-10 flex flex-col gap-6 md:gap-10'>
-        <WeeklyTitle
-          selectedYear={selectedYear}
-          selectedQuarter={selectedQuarter}
-          setSelectedYear={setSelectedYear}
-          setSelectedQuarter={setSelectedQuarter}
+    <div className='flex flex-col gap-6 md:gap-10'>
+      <WeeklyTitle
+        selectedYear={selectedYear}
+        selectedQuarter={selectedQuarter}
+        setSelectedYear={setSelectedYear}
+        setSelectedQuarter={setSelectedQuarter}
+      />
+      {isMobile ? (
+        <MobileDailyConatiner
+          dailyList={WEEKLY_DUMMY[selectedDay]}
+          selectedDay={selectedDay}
+          setSelectedDay={setSelectedDay}
         />
-        {isMobile ? (
-          <MobileDailyConatiner
-            dailyList={WEEKLY_DUMMY[selectedDay]}
-            selectedDay={selectedDay}
-            setSelectedDay={setSelectedDay}
-          />
-        ) : (
-          <WeeklyContainer weeklyList={WEEKLY_DUMMY} currentDay={selectedDay} />
-        )}
-      </div>
-    </MainLayout>
+      ) : (
+        <WeeklyContainer weeklyList={WEEKLY_DUMMY} currentDay={selectedDay} />
+      )}
+    </div>
   );
 };
 
