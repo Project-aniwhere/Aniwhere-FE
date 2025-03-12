3300ba85a68d268d3513dfd01a3b95b527083fdc
leeguts <rmstjd333@gmail.com>
2025-03-12 23:06:22 +0900

    [FEAT] 검색 바 추가

--- src/component/tag/tag-search-bar.tsx
+++ src/component/tag/tag-search-bar.tsx
@@ -2,7 +2,8 @@ import SearchIconSvg from '@/asset/svg/search/search-icon-svg';
 import DefaultInput from '../common/input/default-input';
 import ControlIconSvg from '@/asset/svg/control/control-icon-svg';
 import { TagFilterAction, TagFilterState } from './tag-filter-reducer';
-import { Dispatch } from 'react';
+import { ChangeEvent, Dispatch, useMemo } from 'react';
+import { debounce } from '@/util/throttle';
 
 interface TagSearchBarProps {
   filterState: TagFilterState;
@@ -15,6 +16,16 @@ export default function TagSearchBar({
   dispatch,
   filterState,
 }: TagSearchBarProps) {
+  const handleKeywordChange = useMemo(
+    () =>
+      debounce(
+        (e: ChangeEvent<HTMLInputElement>) =>
+          dispatch({ type: 'SEARCH', payload: e.target.value }),
+        500
+      ),
+    [dispatch]
+  );
+
   return (
     <>
       <div className='z-40 w-[calc(100%+1rem)] -translate-x-2 p-2 bg-white sticky top-[calc(5rem-1px)] md:top-16 flex items-stretch gap-4 h-12'>
@@ -27,9 +38,7 @@ export default function TagSearchBar({
             className='w-full h-full pl-12 pr-4 outline-none border-aniviolet3'
             placeholder='검색어를 입력하세요.'
             value={filterState.searchKeyword}
-            onChange={(e) =>
-              dispatch({ type: 'SEARCH', payload: e.target.value })
-            }
+            onChange={handleKeywordChange}
           />
         </div>
         <button
--- src/hook/query/tag/index.ts
+++ src/hook/query/tag/index.ts
@@ -1,7 +1,35 @@
 import { getSearchedAnime } from '@/action/tag';
 import { TagFilterState } from '@/component/tag/tag-filter-reducer';
 import { AnimeSearchResponse } from '@/type/api/tag-api';
-import { createInfiniteQuery } from '@/util/query-key';
+import { createInfiniteQuery, createQuery } from '@/util/query-key';
+
+const query = createQuery(['tag'], {
+  search: ({
+    searchKeyword,
+    tag,
+    release,
+    season,
+    broadcasting,
+    size = 10,
+  }: Partial<TagFilterState> & {
+    size?: number;
+  }) => {
+    return {
+      queryKey: [searchKeyword, tag, release, season, broadcasting, size],
+      queryFn: async () => {
+        return getSearchedAnime({
+          searchKeyword,
+          tag,
+          release,
+          season,
+          broadcasting,
+          page: 0,
+          size,
+        });
+      },
+    };
+  },
+});
 
 const infiniteQuery = createInfiniteQuery(['tag'], {
   search: ({
@@ -37,6 +65,7 @@ const infiniteQuery = createInfiniteQuery(['tag'], {
 });
 
 const tagQuery = {
+  query,
   infiniteQuery,
 };
 
