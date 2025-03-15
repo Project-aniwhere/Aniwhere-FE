8b8c5daf93441df14958a21422fa0fe69c2fbbc9
yujinyny <hyjinny00@gmail.com>
2025-03-15 21:16:58 +0900

    [FEAT] StarRating 컴포넌트 추가 #

--- src/component/detail/rating.tsx
+++ src/component/detail/rating.tsx
@@ -1,26 +1,34 @@
-import EmptyStar from '@/asset/svg/star/empty-star-svg';
 import HoverColorButton from '../common/button/hover-color-button';
+import { useState } from 'react';
+import StarRating from '../common/star-rating/star-rating';
 
 const Rating = () => {
+  const [rating, setRating] = useState(0);
+  const [content, setContent] = useState('');
+
+  const handleSubmit = () => {
+    console.log(rating, content);
+  };
+
   return (
     <div className='flex flex-col gap-4 p-3 md:p-5'>
       <div className='flex flex-col gap-3'>
         <p className='text-sm md:text-base font-medium'>내 별점</p>
-        <div className='flex gap-2.5 text-xl md:text-3xl font-medium'>
-          <EmptyStar fill='#613DC1' width='2.5rem' height='2.5rem' />
-          <EmptyStar fill='#613DC1' width='2.5rem' height='2.5rem' />
-          <EmptyStar fill='#613DC1' width='2.5rem' height='2.5rem' />
-          <EmptyStar fill='#613DC1' width='2.5rem' height='2.5rem' />
-          <EmptyStar fill='#613DC1' width='2.5rem' height='2.5rem' />
-        </div>
+        <StarRating rating={rating} setRating={setRating} />
       </div>
       <div className='p-4 flex flex-col gap-2 border border-gray-200 rounded-md'>
         <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
           placeholder='이 작품에 대한 내 코멘트를 남겨보세요!'
           className='p-2 resize-none focus-visible:outline-aniviolet2'
         />
         <div className='flex justify-end'>
-          <HoverColorButton text='등록' className='px-3 py-1' />
+          <HoverColorButton
+            text='등록'
+            className='px-3 py-1'
+            onClick={handleSubmit}
+          />
         </div>
       </div>
     </div>
