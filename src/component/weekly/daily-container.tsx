57255afc023cfd330d66045bfd8b747d6fa8de00
yujinyny <hyjinny00@gmail.com>
2025-03-18 21:35:37 +0900

    [REFACTOR] 요일별 애니 썸네일 이미지 fill 적용 #

--- src/component/weekly/daily-container.tsx
+++ src/component/weekly/daily-container.tsx
@@ -18,7 +18,7 @@ const DailyConatiner = ({ day, dailyList, active }: DailyConatinerProps) => {
       >
         {DAYS[day]}
       </p>
-      <ul className='flex flex-col gap-3'>
+      <ul className='flex flex-col gap-4'>
         {dailyList.map((item) => (
           <li key={item.animeId} className='cursor-pointer'>
             <DailyItem data={item} />
--- src/component/weekly/daily-item.tsx
+++ src/component/weekly/daily-item.tsx
@@ -8,18 +8,17 @@ interface DailyItemProps {
 const DailyItem = ({ data }: DailyItemProps) => {
   return (
     <div className='flex flex-col gap-2'>
-      {data.poster ? (
-        <AWImage
-          src={data.poster}
-          alt={`${data.title} 포스터`}
-          width={384}
-          height={543}
-          sizes='(min-width: 768px) 15vw, 33vw'
-          className='rounded hover:scale-105 duration-200'
-        />
-      ) : (
-        <div className='w-full h-[200px] rounded hover:scale-105 duration-200 bg-gray-300'></div>
-      )}
+      <div className='relative w-full h-[150px] md:h-[250px] rounded hover:scale-105 duration-200 bg-gray-200'>
+        {data.poster && (
+          <AWImage
+            src={data.poster}
+            alt={`${data.title} 포스터`}
+            fill
+            sizes='(min-width: 768px) 14vw, 33vw'
+            className='rounded object-cover'
+          />
+        )}
+      </div>
       <p className='font-medium truncate text-sm md:text-base'>{data.title}</p>
     </div>
   );
