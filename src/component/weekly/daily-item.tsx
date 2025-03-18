e102948e5638cf134817cc07dc85d6d20312753a
yujinyny <hyjinny00@gmail.com>
2025-03-18 21:15:50 +0900

    [REFACTOR] 요일별 애니 썸네일 없을 시 gray-box로 보여줌 #

--- src/component/weekly/daily-item.tsx
+++ src/component/weekly/daily-item.tsx
@@ -8,14 +8,18 @@ interface DailyItemProps {
 const DailyItem = ({ data }: DailyItemProps) => {
   return (
     <div className='flex flex-col gap-2'>
-      <AWImage
-        src={data.poster || `${data.title} 포스터`}
-        alt={`${data.title} 포스터`}
-        width={384}
-        height={543}
-        sizes='(min-width: 768px) 15vw, 33vw'
-        className='rounded hover:scale-105 duration-200'
-      />
+      {data.poster ? (
+        <AWImage
+          src={data.poster}
+          alt={`${data.title} 포스터`}
+          width={384}
+          height={543}
+          sizes='(min-width: 768px) 15vw, 33vw'
+          className='rounded hover:scale-105 duration-200'
+        />
+      ) : (
+        <div className='w-full h-[200px] rounded hover:scale-105 duration-200 bg-gray-300'></div>
+      )}
       <p className='font-medium truncate text-sm md:text-base'>{data.title}</p>
     </div>
   );
