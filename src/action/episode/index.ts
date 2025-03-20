82b4e4aaa9f806470c5448050414ebc46c9dbbe5
yujinyny <hyjinny00@gmail.com>
2025-03-20 15:15:13 +0900

    [FEAT] 리뷰 작성 에러핸들링 #

--- src/action/episode/index.ts
+++ src/action/episode/index.ts
@@ -31,8 +31,7 @@ export const postEpisodeReview = async (
     }
   );
 
-  if (response.ok) return null;
-  return null;
+  return response.json();
 };
 
 // 에피소드 리뷰 수정
@@ -52,8 +51,7 @@ export const putEpisodeReview = async (
     }
   );
 
-  if (response.ok) return null;
-  return null;
+  return response.json();
 };
 
 // 에피소드 리뷰 삭제
--- src/component/detail/rating.tsx
+++ src/component/detail/rating.tsx
@@ -1,5 +1,5 @@
 import HoverColorButton from '../common/button/hover-color-button';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { putAnimeReview } from '@/action/anime';
 import { useAtomValue } from 'jotai';
 import { sessionAtom } from '@/store/session-atom';
@@ -7,6 +7,8 @@ import StarRate from '../common/star-rate/star-rate';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { postEpisodeReview } from '@/action/episode';
 import { PageType } from '@/type/api/anime-api';
+import ModalDialog from '../common/modal/modal-dialog';
+import { ModalRef } from '@/type/modal';
 
 interface RatingProps {
   id: string;
@@ -19,10 +21,12 @@ const Rating = ({ id, type }: RatingProps) => {
   const session = useAtomValue(sessionAtom);
   const disabled = !session.isLogin;
 
+  const modalRef = useRef<ModalRef>(null);
+
   const [rating, setRating] = useState(0);
   const [content, setContent] = useState('');
 
-  const { mutate: handleSubmit } = useMutation({
+  const { mutate: handleSubmit, data } = useMutation({
     mutationFn: () =>
       type === 'anime'
         ? putAnimeReview(id, session.userInfo?.userId || 0, {
@@ -33,44 +37,55 @@ const Rating = ({ id, type }: RatingProps) => {
             rating,
             content,
           }),
-    onSuccess: () => {
-      if (type === 'anime')
-        queryClient.invalidateQueries({ queryKey: ['animeDetail', id] });
-      else queryClient.invalidateQueries({ queryKey: ['episodeDetail', id] });
-      setRating(0);
-      setContent('');
+    onSuccess: (data) => {
+      if (data?.code === 200) {
+        if (type === 'anime') {
+          queryClient.invalidateQueries({ queryKey: ['animeDetail', id] });
+        } else {
+          queryClient.invalidateQueries({ queryKey: ['episodeDetail', id] });
+          setRating(0);
+          setContent('');
+        }
+      } else {
+        modalRef.current?.openModal();
+      }
     },
   });
 
   return (
-    <div className='flex flex-col gap-4 p-3 md:p-5'>
-      <div className='flex flex-col gap-3'>
-        <p className='md:text-lg font-bold'>내 평점</p>
-        <StarRate
-          rate={rating}
-          setRate={setRating}
-          disabled={disabled}
-          size='2.4rem'
-        />
-      </div>
-      <div className='p-4 flex flex-col gap-2 border border-gray-200 rounded-md'>
-        <textarea
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          placeholder={`${disabled ? '로그인 후 ' : ''}이 작품에 대한 리뷰를 남겨보세요!`}
-          className='p-2 resize-none focus-visible:outline-aniviolet2'
-          disabled={disabled}
-        />
-        <div className='flex justify-end'>
-          <HoverColorButton
-            text='등록'
-            className='px-3 py-1'
-            onClick={() => handleSubmit()}
+    <>
+      <ModalDialog ref={modalRef}>
+        <div>{data?.message}</div>
+      </ModalDialog>
+      <div className='flex flex-col gap-4 p-3 md:p-5'>
+        <div className='flex flex-col gap-3'>
+          <p className='md:text-lg font-bold'>내 평점</p>
+          <StarRate
+            rate={rating}
+            setRate={setRating}
             disabled={disabled}
+            size='2.4rem'
           />
         </div>
+        <div className='p-4 flex flex-col gap-2 border border-gray-200 rounded-md'>
+          <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            placeholder={`${disabled ? '로그인 후 ' : ''}이 작품에 대한 리뷰를 남겨보세요!`}
+            className='p-2 resize-none focus-visible:outline-aniviolet2'
+            disabled={disabled}
+          />
+          <div className='flex justify-end'>
+            <HoverColorButton
+              text='등록'
+              className='px-3 py-1'
+              onClick={() => handleSubmit()}
+              disabled={disabled}
+            />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
